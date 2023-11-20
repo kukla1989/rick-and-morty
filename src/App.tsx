e3c5fe77e4b0300df0e572471a8293fd91a01213
kukla1989 <kukla.1989@ukr.net>
2023-11-20 09:41:12 +0200

    inialize react + ts + sass + eslint + prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+import { Main } from './Main/Main';
+
+export const App: React.FC = () => {
+  return (
+    <div className="starter">
+      <Main />
+
+    </div>
+  );
+};
---
+++ src/Main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Main = () => (
+  <div className="main">
+    <h1>
+      from m
+    </h1>
+  </div>
+);
